e87deff5ae07babd0f294fe23b19fc93249d6bca
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2019-08-19 02:57:19 +0300

    texture added

--- CubeModel/RotatingCube.cpp
+++ CubeModel/RotatingCube.cpp
@@ -1,64 +1,128 @@
 #include <GL/glut.h>
+//#include <GL/glaux.h>
+#include <stdio.h>
 
 bool fullscreen = false;
 bool mouseDown = false;
 
 float xrot = 0.0f;
 float yrot = 0.0f;
-
 float xdiff = 0.0f;
 float ydiff = 0.0f;
+GLuint texture[1];
+
+GLuint LoadTexture(const char *filename)
+{
+    GLuint texture;
+    int width, height;
+    unsigned char *data;
+    FILE *file;
+    file = fopen(filename, "rb");
+    if (file == NULL) return 0;
+    width = 256;
+    height = 256;
+    data = (unsigned char *) malloc(width * height * 3);
+    fread(data, width * height * 3, 1, file);
+    fclose(file);
+
+    for (int i = 0; i < width * height; ++i)
+    {
+        int index = i * 3;
+        unsigned char B, R;
+        B = data[index];
+        R = data[index + 2];
+
+        data[index] = R;
+        data[index + 2] = B;
+
+    }
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
+    free(data);
+
+    return texture;
+}
 
 void drawBox()
 {
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
     glBegin(GL_QUADS);
 
-    glColor3f(1.0f, 0.0f, 0.0f);
-// FRONT
+    glTexCoord2f(0.0f, 0.0f);
     glVertex3f(-0.5f, -0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.0f);
     glVertex3f(0.5f, -0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.5f);
     glVertex3f(0.5f, 0.5f, 0.5f);
+    glTexCoord2f(0.0f, 0.5f);
     glVertex3f(-0.5f, 0.5f, 0.5f);
-// BACK
+
+    glTexCoord2f(0.5f, 0.0f);
     glVertex3f(-0.5f, -0.5f, -0.5f);
+    glTexCoord2f(0.5f, 0.5f);
     glVertex3f(-0.5f, 0.5f, -0.5f);
+    glTexCoord2f(0.0f, 0.5f);
     glVertex3f(0.5f, 0.5f, -0.5f);
+    glTexCoord2f(0.0f, 0.0f);
     glVertex3f(0.5f, -0.5f, -0.5f);
 
-    glColor3f(0.0f, 1.0f, 0.0f);
-// LEFT
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
+    glTexCoord2f(0.0f, 0.5f);
     glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-// RIGHT
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-0.5f, 0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.0f);
     glVertex3f(0.5f, 0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.5f);
+    glVertex3f(0.5f, 0.5f, -0.5f);
+
+    glTexCoord2f(0.5f, 0.5f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glTexCoord2f(0.0f, 0.5f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+
+    glTexCoord2f(0.5f, 0.0f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    glTexCoord2f(0.5f, 0.5f);
+    glVertex3f(0.5f, 0.5f, -0.5f);
+    glTexCoord2f(0.0f, 0.5f);
+    glVertex3f(0.5f, 0.5f, 0.5f);
+    glTexCoord2f(0.0f, 0.0f);
     glVertex3f(0.5f, -0.5f, 0.5f);
 
-    glColor3f(0.0f, 0.0f, 1.0f);
-// TOP
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-// BOTTOM
-    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glTexCoord2f(0.0f, 0.0f);
     glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glTexCoord2f(0.5f, 0.5f);
+    glVertex3f(-0.5f, 0.5f, 0.5f);
+    glTexCoord2f(0.0f, 0.5f);
+    glVertex3f(-0.5f, 0.5f, -0.5f);
+
     glEnd();
 }
 
 bool init()
 {
+    texture[0] = LoadTexture("C:/Users/Timoniche/Desktop/Graphics/CubeModel/TallGreenGrass.bmp");
+    glEnable(GL_TEXTURE_2D);
+
     glClearColor(0.93f, 0.93f, 0.93f, 0.0f);
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
     glClearDepth(1.0f);
-
     return true;
 }
 
@@ -100,7 +164,8 @@ void keyboard(unsigned char key, int x, int y)
     {
         case 27 :
             exit(1);
-        default:break;
+        default:
+            break;
     }
 }
 

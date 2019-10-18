f55d1ea37113b026acec7d4a7ff57912ee7d9682
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2019-10-18 03:43:51 +0300

    simple bbox&clipping

---
+++ CubeBresenham/cubeQT/geom_test.cpp
@@ -0,0 +1,6 @@
+#include "geom_test.h"
+
+geom_test::geom_test()
+{
+
+}
---
+++ CubeBresenham/cubeQT/geom_test.h
@@ -0,0 +1,11 @@
+#ifndef GEOM_TEST_H
+#define GEOM_TEST_H
+
+
+class geom_test
+{
+public:
+    geom_test();
+};
+
+#endif // GEOM_TEST_H

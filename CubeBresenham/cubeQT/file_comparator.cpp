49998af7e63f885fc5896dbb191aad2bf97e800d
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2019-11-13 14:09:25 +0300

    change file comparator

--- CubeBresenham/cubeQT/file_comparator.cpp
+++ CubeBresenham/cubeQT/file_comparator.cpp
@@ -23,22 +23,22 @@ int main()
     //freopen(file1, "r", stdin);
     ifstream f1(file1);
     ifstream f2(file2);
-    vector<pii> first;
-    vector<pii> second;
-    int in, in2;
+    vector<tuple<int, int, int, int, int>> first;
+    vector<tuple<int, int, int, int, int>> second;
+    int in, in2, in3, in4, in5;
     while (f1 >> in) {
-        f1 >> in2;
-        first.emplace_back(in, in2);
+        f1 >> in2 >> in3 >> in4 >> in5;
+        first.emplace_back(in, in2, in3, in4, in5);
     }
     //fclose(stdin);
     freopen(file2, "r", stdin);
     while (f2 >> in) {
-        f2 >> in2;
-        second.emplace_back(in, in2);
+        f2 >> in2 >> in3 >> in4 >> in5;
+        second.emplace_back(in, in2, in3, in4, in5);
     }
     std::sort(first.begin(), first.end());
     std::sort(second.begin(), second.end());
-    vector<pii> inter;
+    vector<tuple<int, int, int, int, int>> inter;
     std::set_intersection(first.begin(),first.end(),
                           second.begin(),second.end(),
                           back_inserter(inter));
